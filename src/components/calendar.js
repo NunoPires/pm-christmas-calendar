082ac51e10e98394423cd99f29acf46960129b16
Nuno Pires <nuno.pires@premium-minds.com>
2020-11-11 15:03:09 +0000

    limit calendar days

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -4,10 +4,10 @@ import {CalendarItem} from './calendar-day.js'
 
 export class CalendarLayout extends Component {
     
-    getCalendarDays(month, year) {
+    getCalendarDays(month, year, dayLimit) {
         var days = [];
         var date = new Date(year, month, 1);
-        while(date.getMonth() === month) {
+        while(date.getMonth() === month && date.getDate() !== dayLimit) {
             days.push(date.getDate());
             date.setDate(date.getDate() + 1);
         }
@@ -17,7 +17,7 @@ export class CalendarLayout extends Component {
     render() {
 
         // 0=January, 11 = December
-        let days = _.map(this.getCalendarDays(11, 2020), (day) => {
+        let days = _.map(this.getCalendarDays(11, 2020, 25), (day) => {
             return <CalendarItem key={day} day={day} />   
         });
         
