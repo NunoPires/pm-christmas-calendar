1609d2ef15a45901d1295e3573c88ffb6872b4a8
Nuno Pires <nuno.pires@premium-minds.com>
2020-11-17 14:59:34 +0000

    added local storage state for viewed items

--- src/components/calendar-item.js
+++ src/components/calendar-item.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import ls from 'local-storage';
 import {CalendarItemDetail} from './calendar-item-detail.js';
 
 export class CalendarItem extends Component {
 
     constructor() {
         super();
-        this.state = {showDetail: false};
+        this.state = {showDetail: false, viewed: false};
+    }
+
+    componentDidMount() {
+        this.setState({viewed: ls.get(this.props.day) || false});
     }
 
     handleKeyDown(event) {
@@ -15,25 +20,31 @@ export class CalendarItem extends Component {
     }
 
     toggleDetail() {
-        this.setState({showDetail: !this.state.showDetail});
+        this.setState({showDetail: !this.state.showDetail, viewed: true});
+        ls.set(this.props.day, true);
     }
 
     fetchCalendarItemDetail() {
         //let detail = getDetail(this,props.day);
         let detail = {
-            title: "Teste", 
+            title: "It's beginning to look like Christmas", 
             description: "É Natal, tudo é mágico e portanto, toma uma surpresa!",
             imageURL: window.location.origin + "/test-image.jpg",
-            destinationURL: "https://www.premium-minds.com"
+            destinationURL: "https://fun.premium-minds.com/#15950027408812/15952898613592"
         };
         return detail;
     }
 
     render() {
+        let className = "col-md-2 calendar-item";
+        if(this.state.viewed) {
+            className+= "-viewed";
+        }
+
         let currentDay = this.props.day;
         let content = this.fetchCalendarItemDetail();
         return (
-            <div tabIndex={1} className="col-md-2 calendar-item" onKeyDown={this.handleKeyDown.bind(this)} onClick={this.toggleDetail.bind(this)}>
+            <div tabIndex={1} className={className} onKeyDown={this.handleKeyDown.bind(this)} onClick={this.toggleDetail.bind(this)}>
                 {currentDay}
                 <CalendarItemDetail 
                     day={currentDay} 
