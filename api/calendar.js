efa4ebcab957e2ff49d7783a975e61ed60f77e5d
dfigueiredo <dfigueiredo@pminds.pt>
2020-11-20 16:32:13 +0000

    Add Calendar API and data

---
+++ api/calendar.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
+module.exports = (req, res) => {
+
+    const DATE_LIMIT = new Date("2020-12-03")
+    //const DATE_LIMIT = new Date()
+
+    const rawdata = readFileSync(join(__dirname, 'data.json'), 'utf8')
+    let calendar = JSON.parse(rawdata);
+    let filteredCalendar = []
+
+    for(const [key, val] of Object.entries(calendar)) {
+        let itemDate = new Date(val.date)
+
+        if (itemDate <= DATE_LIMIT) {
+            filteredCalendar.push(val)
+        }
+    };
+
+    res.setHeader('Cache-Control', 's-maxage=60')
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(JSON.stringify(filteredCalendar))
+}
